00741f2c737354d8d95cb05bd531fac7331b1793
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-04 22:22:29 +0200

    Start adding better-auth

---
+++ src/server/auth/auth.ts
@@ -0,0 +1,13 @@
+// TODO rename to index.ts
+import { db } from "@/server/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
