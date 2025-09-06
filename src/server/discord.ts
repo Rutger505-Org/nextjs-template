171d798248d615abe296cb544169646050b3bfec
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-07 00:41:00 +0200

    Make discord messages be sendable from the client again. and improve logging

--- src/server/discord.ts
+++ src/server/discord.ts
@@ -1,4 +1,4 @@
-import "server-only";
+"use server";
 
 import { env } from "@/env";
 import axios from "axios";
@@ -6,12 +6,14 @@ import axios from "axios";
 export async function sendDiscordMessage(message: string) {
   if (!env.DISCORD_WEBHOOK_URL) {
     console.warn(
-      "DISCORD_WEBHOOK_URL is not set. Not sending Discord message.",
+      "DISCORD_WEBHOOK_URL is not set. Not sending Discord message:",
+      message,
     );
     return;
   }
 
   try {
+    console.log("Discord message:", message);
     await axios.post(env.DISCORD_WEBHOOK_URL, {
       content: message,
     });
